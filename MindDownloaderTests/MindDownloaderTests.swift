1e7a9fc4ca3239c64d70c1dd8ef2f7e73760c042
ahmed.samir <ahmed.samir@ibtikar.net.sa>
2020-01-07 20:27:27 +0200

    [Technical]: Adding Unit Test Bundle For MindDownloader Library

---
+++ MindDownloaderTests/MindDownloaderTests.swift
@@ -0,0 +1,21 @@
+//
+//  MindDownloaderTests.swift
+//  MindDownloaderTests
+//
+//  Created by Ahmed Samir on 1/7/20.
+//  Copyright © 2020 Tania. All rights reserved.
+//
+
+import XCTest
+
+class MindDownloaderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
